6ec3b1892b967cbb09bcb27f240820769d1f80d3
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-07-25 19:31:41 +0200

    Addded colors

--- modules/time.js
+++ modules/time.js
@@ -2,6 +2,6 @@ function time(uptime){
 	var hrs = parseInt(uptime/3600);
 	var min = parseInt((uptime- hrs*3600)/60);
 	var sec = (uptime - hrs*3600 -min*60).toFixed(0);
-	console.log('Uptime:', hrs, 'h', min, 'm', sec, 's')
+	console.log('Uptime:'.green, hrs, 'h', min, 'm', sec, 's')
 };
 exports.print = time;
